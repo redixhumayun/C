6b1d796773f52d06db44552f2acc3881e6acb570
Zaid Humayun <redixhumayun@gmail.com>
2017-10-29 22:40:53 +0530

    New commit

---
+++ 31/ex31.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int arc, char *argv[])
+{
+  int i = 0;
+
+  char *pointer;
+
+  for(i = 0; i < 5; i++) {
+    printf("%c", pointer[i]);
+  }
+
+  return 0;
+}

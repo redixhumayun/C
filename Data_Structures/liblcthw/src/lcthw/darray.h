72999ff8f35ed759b292d3ba0ec532b377026592
Zaid Humayun <zaidhumayun@Zaids-MacBook-Pro.local>
2017-12-18 11:06:36 +0530

    dynamic array

---
+++ Data_Structures/liblcthw/src/lcthw/darray.h
@@ -0,0 +1,30 @@
+#ifndef _DArray_h
+#define _DArray_h
+#include <stdlib.h>
+#include <assert.h>
+#include <lcthw/dbg.h>
+
+typedef struct DArray {
+  int end;
+  int max;
+  size_t element_size;
+  size_t expand_rate;
+  void **contents;
+} DArray;
+
+DArray *DArray_create(size_t element_size, size_t initial_max);
+
+void DArray_Destroy(DArray *array);
+
+void DArray_clear(DArray *array);
+
+void DArray_expand(DArray *array);
+
+int DArray_push(DArray *array, void *el);
+
+void *DArray_pop(DArray *array);
+
+void DArray_clear_destroy(DArray *array);
+
+#define DArray_last(A) ((A)->contents[(A)->end - 1])
+#define 

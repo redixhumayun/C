b99a11f5be1295bdf2e59606b00695f93efd77a0
Zaid Humayun <redixhumayun@gmail.com>
2018-03-05 15:11:30 +0530

    check for same binary trees

--- New_Data_Structures/BinaryTree/binary_tree_algos.c
+++ New_Data_Structures/BinaryTree/binary_tree_algos.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "./binary_tree.h"
-#define MAXVALUE 100000000
+#define MAXdata 100000000
 
 int findMin(int arr[], int beg, int end) {
   int i = 0;
-  int min = MAXVALUE;
+  int min = MAXdata;
   for(i = beg; i < end; i++) {
     if(arr[i] < min) {
       min = arr[i];
@@ -52,10 +52,38 @@ int getMin(Node *root, int beg, int end, int arr_size) {
   }
 }
 
+//function to check whether two binary trees are identical
+int checkSameTrees(Node *root_1, Node *root_2) {
+  //both are empty
+  if(root_1 == NULL && root_2 == NULL) {
+    return 1;
+  }
+  //both are not empty
+  else if(root_1 != NULL && root_2 != NULL) {
+    return (
+      root_1->data == root_2->data &&
+      checkSameTrees(root_1->left, root_2->left) && 
+      checkSameTrees(root_1->right, root_2->right)
+    );
+  }
+  //one is empty and one is not
+  else {
+    return 0;
+  }
+}
+
 int main(int argc, char *argv[]) {
-  int arr[] = {4, 5, 7, 2, 3, 1, 9, 10};
-  Node *root = createNode(1);
-  createTreeHelper(arr, 0, 8, root);
-  int result = getMin(root, 0, 1, 8);
-  printf("Result: %d\n", result);
+  int arr[] = {4, 5, 7, 2, 3, 9, 10};
+  int arr_2[] = {2,4,5,7};
+  Node *root_1 = createNode(1);
+  Node *root_2 = createNode(1);
+  int i = 0;
+  for(i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+    insertNode(root_1, createNode(arr[i]));
+  }
+  for(i = 0; i < sizeof(arr_2) / sizeof(arr_2[0]); i++) {
+    insertNode(root_2, createNode(arr_2[i]));
+  }
+  int result = checkSameTrees(root_1, root_2);
+  printf("Value: %d\n", result);
 }
